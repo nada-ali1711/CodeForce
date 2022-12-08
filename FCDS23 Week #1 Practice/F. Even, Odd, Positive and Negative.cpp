a3ab7a330174e4cedd7d13d24b8563fa809678c3
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2022-12-08 10:58:58 +0200

    Update F. Even, Odd, Positive and Negative.cpp

--- FCDS23 Week #1 Practice/F. Even, Odd, Positive and Negative.cpp
+++ FCDS23 Week #1 Practice/F. Even, Odd, Positive and Negative.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m,even_i=0,n_count=0,odd_i=0;
+    int p_count=0;
+    cin>>n;
+    for (int i=1;i<=n;i++){
+    cin>>m;
+    if (m%2==0)
+    even_i+=1;
+    else
+    odd_i+=1;
+    if (m>0)
+    p_count+=1;
+    if (m<0)
+    n_count+=1;
+
+    }
+    cout<<"Even: "<<even_i<<endl;
+    cout<<"Odd: "<<odd_i<<endl;
+    cout<<"Positive: "<<p_count<<endl;
+    cout<<"Negative: "<<n_count<<endl;
+    return 0;
+}
