848c1fe747ed47e94682a5155cc41f9efe4c64d1
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2022-12-08 10:17:11 +0200

    Create E. Even Numbers.cpp

---
+++ FCDS23 Week #1 Practice/E. Even Numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i=1;
+    bool x=i%2==0;
+    cin>>n;
+    for (i=1;i<=n;i++){
+    if (i%2==0){
+    cout<<i<<endl;
+    }}
+    if (n==1)
+    cout<<-1;
+
+
+
+
+    return 0;
+}
