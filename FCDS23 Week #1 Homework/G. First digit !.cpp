6056b4c3fc2abb64c60f4c0e63f73ce8e77294a9
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2022-12-09 10:15:16 +0200

    Add files via upload

---
+++ FCDS23 Week #1 Homework/G. First digit !.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+
+    cin>>x;
+    y=x/1000;
+    if(y%2==0)
+    cout<<"EVEN";
+    else
+    cout<<"ODD";
+    return 0;
+}
