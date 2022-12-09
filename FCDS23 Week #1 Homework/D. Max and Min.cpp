100fe6ba373bb19e890026c3393cca873f02f092
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2022-12-09 10:08:19 +0200

    Add files via upload

---
+++ FCDS23 Week #1 Homework/D. Max and Min.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a, b, c ;
+
+    cin>>a>>b>>c;
+    if (a>=b && b>=c)
+    cout<<c<<" "<<a;
+    else if (a>=c && c>=b)
+    cout<<b<<" "<<a;
+    else if (b>=a && a>=c)
+    cout<<c<<" "<<b;
+    else if (b>=c && c>=a)
+    cout<<a<<" "<<b;
+    else if (c>=a && a>=b)
+    cout<<b<<" "<<c;
+    else if (c>=b && b>=a)
+    cout<<a<<" "<<c;
+    return 0;
+}
