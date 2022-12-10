6aa15e89e382c25d94f8217848bff9fec6c2ec09
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2022-12-10 12:04:31 +0200

    Add files via upload

---
+++ FCDS23 Week #2 Homework/F. Shape1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i,m,j;
+    cin>>n;
+    for (j=0;j<n;n--){
+    for (i=0;i<n;i++){
+        cout<<"*";
+    }
+    cout<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
